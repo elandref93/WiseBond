c22505cf47aafde53812d0bcb62ffe32f521c389
rxjitendra <jitendra@eapfs.co.za>
2025-06-26 18:00:28 +0530

    added package-lock.json file

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
-const { ManagedIdentityCredential } = require("@azure/identity");
-const { Client } = require("pg");
+import { ManagedIdentityCredential } from "@azure/identity";
+import { Client } from "pg";
  
 export async function getPostgresClient() {
     const credential = new ManagedIdentityCredential();
     const host = process.env["database_host"]?.trim();
     const database = process.env["database_name"]?.trim();
     const user = process.env["database_user"]?.trim();
-    const port = process.env["database_port"]?.trim();
+    const port = parseInt(process.env["database_port"]?.trim() || "5432", 10);
  
     console.log(host)
     console.log(database)
